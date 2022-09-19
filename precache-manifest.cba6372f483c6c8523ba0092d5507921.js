7adcf100f8e631e43ced8f206605df587fbd78b5
vagrant <eng.saad.najm@gmail.com>
2022-09-19 03:53:52 +0000

    Updates

---
+++ precache-manifest.cba6372f483c6c8523ba0092d5507921.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "84b775fe2f146d0bf6fe",
+    "url": "/LEARNAZ/static/css/main.0eda507e.chunk.css"
+  },
+  {
+    "revision": "84b775fe2f146d0bf6fe",
+    "url": "/LEARNAZ/static/js/main.2a514aa0.chunk.js"
+  },
+  {
+    "revision": "22b6a6df214244a8ee4b",
+    "url": "/LEARNAZ/static/js/runtime~main.a0731315.js"
+  },
+  {
+    "revision": "39e0aa3246419fb7b555",
+    "url": "/LEARNAZ/static/css/2.f7ae71d9.chunk.css"
+  },
+  {
+    "revision": "39e0aa3246419fb7b555",
+    "url": "/LEARNAZ/static/js/2.57b71323.chunk.js"
+  },
+  {
+    "revision": "1b7a6cca6f2ee9581abbc4f760c8ba61",
+    "url": "/LEARNAZ/index.html"
+  }
+];
